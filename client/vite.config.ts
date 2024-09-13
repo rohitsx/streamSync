af8d8aa9ffb23d96a7d46d4c88fb11ec40683e46
rohit <rohiitrb@gmail.com>
2024-09-13 20:57:06 +0530

    updated home page ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      process: "process/browser"
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
