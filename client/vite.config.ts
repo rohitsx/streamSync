9a29eeda2e2fb36018002a73556975065a055637
rohit <rohiitrb@gmail.com>
2024-12-18 13:25:08 +0530

    added crxjs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,39 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 
+const manifest = defineManifest({
+  version: "1.0.0",
+  manifest_version: 3,
+  name: "StreamSync",
+  description: "This is a Chrome extension built with React and TypeScript",
+  action: {
+    default_popup: "./index.html",
+  },
+  icons: {
+    "128": "src/assets/icon128.png",
+  },
+  background: {
+    service_worker: "script/background/bg.ts",
+    type: "module",
+  },
+  permissions: ["tabs", "identity", "cookies", "scripting"],
+  host_permissions: ["https://localhost:8000/api/*"],
+});
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./script/shared"),
     },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        popup: path.resolve(__dirname, 'index.html'),
-        content: path.resolve(__dirname, 'src/components/contentScript/content.tsx')
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' http://localhost:* http://127.0.0.1:* https://accounts.google.com",
+    },
+  },
 });
